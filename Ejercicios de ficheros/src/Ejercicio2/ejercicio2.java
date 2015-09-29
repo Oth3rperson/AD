04d87fc9d949e3b6af703a6478aefa8a4e4717a2
administradorces <administradorces@192.168.1.10>
2015-09-29 09:26:21 +0200

    start ejercicio2

---
+++ Ejercicios de ficheros/src/Ejercicio2/ejercicio2.java
@@ -0,0 +1,13 @@
+package Ejercicio2;
+
+import java.io.File;
+
+public class ejercicio2 {
+
+	public static void main(String[] args) {
+		File archivoOrigen = new File("ejercicio1.txt");
+		File archivoDestino = new File("ejercicio2.txt");
+
+	}
+
+}
